c4ed2bd556bd6efeee63a1f1f201044ff1e4e88e
Harish A <harish18092002@gmail.com>
2022-09-23 22:19:39 +0530

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component: AppComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'login',component: LoginComponent},
+      {path: 'signup',component: SignupComponent},
+      {path: 'profile', component: ProfileComponent},
+      {path: '**', redirectTo: 'home'},
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  goTosignup() {
+    this.router.navigate(['./signup']);
+  }
+  goToprofile() {
+    this.router.navigate(['./profile']);
+  }
+
+}
