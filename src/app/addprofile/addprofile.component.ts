ae18996f2b6f21d253f9d609c51512bbaae95068
Harish A <harish18092002@gmail.com>
2022-10-12 19:23:06 +0530

    heroku

--- src/app/addprofile/addprofile.component.ts
+++ src/app/addprofile/addprofile.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-addprofile',
   templateUrl: './addprofile.component.html',
   styleUrls: ['./addprofile.component.css']
 })
 export class AddprofileComponent implements OnInit {
+
+  grid = new FormControl(Validators.required);
+
+
 
   constructor(private router : Router) { }
 
@@ -17,5 +22,9 @@ goTohome(){
 }
 goToprofile(){
   this.router.navigate(['./profile']);
+}
+names:string="";
+setValue(names:string){
+  console.log("Name:",names);
 }
 }
